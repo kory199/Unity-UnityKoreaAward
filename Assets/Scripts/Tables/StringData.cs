62c39f469684615ba539d678812cfae24c6fe78d
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-07-11 15:30:23 +0900

    none

---
+++ Assets/Scripts/Tables/StringData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class StringData
+{
+    public int ID;
+    public string Descript;
+    public string Kor;
+    public static Dictionary<int, StringData> table = new Dictionary<int, StringData> ();   
+}
