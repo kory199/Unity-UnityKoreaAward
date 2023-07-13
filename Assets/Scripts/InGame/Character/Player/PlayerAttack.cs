fccc3210289cfb0beb87ac054e171c63d63105e1
WonChan kim <111823096+KingOneChance@users.noreply.github.com>
2023-07-13 23:14:59 +0900

    Update
    Player Attack :
     GetKeyDown=>GetMouseButtonDown
     (A key problem)
    
    Insert
    TMPro

--- Assets/Scripts/InGame/Character/Player/PlayerAttack.cs
+++ Assets/Scripts/InGame/Character/Player/PlayerAttack.cs
@@ -6,7 +6,11 @@ public partial class Player
 {
     public override void Attack()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+       /* if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject bullet = ObjectPooler.SpawnFromPool("Bullet", gameObject.transform.position);
+        }*/
+        if (Input.GetMouseButtonDown(0)) // A키가 Move와 겹쳐서 마우스 좌클릭으로 테스트 진행해봤어용
         {
             GameObject bullet = ObjectPooler.SpawnFromPool("Bullet", gameObject.transform.position);
         }
