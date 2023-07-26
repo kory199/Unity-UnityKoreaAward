8ef4f9832b93c86e241e1c8ee67a8e3663588ddd
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-07-26 19:59:29 +0900

    Update APIManager.cs
    json array -> list : fail ..

--- Assets/Scripts/API/APIManager.cs
+++ Assets/Scripts/API/APIManager.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using APIModels;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class APIManager : MonoSingleton<APIManager>
@@ -24,13 +25,13 @@ public class APIManager : MonoSingleton<APIManager>
     private int _status;
 
     // Ranking Data
-    private string _rankId;
-    private int _rankScore;
-    private int _ranking;
+    private List<string> _rankIdList = new List<string>();
+    private List<int> _rankScoreList = new List<int>();
+    private List<int> _rankingList = new List<int>();
 
     private void Awake()
     {
-        if(apidata == null)
+        if (apidata == null)
         {
             apidata = Resources.Load<APIDataSO>("APIData");
         }
@@ -64,6 +65,7 @@ public class APIManager : MonoSingleton<APIManager>
         {
             apidata.SetResponseData("GameData", NewGameData());
         }
+
         else
         {
             Debug.LogError("Failed to retrieve authToken from API response.");
@@ -72,17 +74,12 @@ public class APIManager : MonoSingleton<APIManager>
 
     public async UniTask GetGameDataAPI()
     {
-         await CallAPI<Dictionary<string, object>, PlayerData>(APIUrls.GameDataApi, NewPlayerData(), null);
-    }
-
-    private void HandleGameDataResponse(APIResponse<Dictionary<string, object>> apiResponse)
-    {
-        // APIWebRequest.ParseResponseBodyToModel<PlayerData>(apiResponse.responseBody, "playerData");
+        await CallAPI<Dictionary<string, object>, GameData>(APIUrls.GameDataApi, NewGameData(), null);
     }
 
     public async UniTask GetRanking()
     {
-        await CallAPI<Dictionary<string, object>, RankingData>(APIUrls.RankingApi, NewRankingData(), null);
+        await CallAPI<Dictionary<string, object>, GameData>(APIUrls.RankingApi, NewGameData(), null);
     }
 
     private void HandleRankingDataResponse(APIResponse<Dictionary<string, object>> apiResponse)
@@ -100,47 +97,86 @@ public class APIManager : MonoSingleton<APIManager>
         return gameData;
     }
 
-    private PlayerData NewPlayerData()
-    {
-        PlayerData playerData = new PlayerData
-        {
-            player_uid = _player_uid,
-            exp = _exp,
-            hp = _hp,
-            score = _score,
-            level = _level,
-            status = _status
-        };
-        return playerData;
-    }
-
-    private RankingData NewRankingData()
-    {
-        RankingData rankingData = new RankingData
-        {
-            rankId = _rankId,
-            rankScore = _rankScore,
-            ranking = _ranking
-        };
-        return rankingData;
-    }
-
     private async UniTask CallAPI<T, TRequest>(string apiUrl, TRequest requestBody, Action<APIResponse<T>> handler)
     {
         try
         {
             var apiResponse = await APIWebRequest.PostAsync<T>(apiUrl, requestBody);
 
-            if(apiResponse == null)
+            if (apiResponse == null)
             {
-                Debug.LogError("API Respon is null");
+                Debug.LogError("API 응답이 null입니다");
+            }
+            else
+            {
+                string responseBody = apiResponse.responseBody;
+
+                switch (apiUrl)
+                {
+                    case var url when url == APIUrls.GameDataApi:
+                        
+                        break;
+                    case var url when url == APIUrls.RankingApi:
+                        await AddRankingDataAsync(apiResponse.responseBody);
+                        break;
+                    default:
+                        break;
+                }
             }
 
             handler?.Invoke(apiResponse);
         }
+
         catch (UnityWebRequestException e)
         {
             Debug.LogError($"API request failed : {e.Message}");
+        }
+    }
+    /*
+        private PlayerData NewPlayerData()
+        {
+            PlayerData playerData = new PlayerData
+            {
+                player_uid = _player_uid,
+                exp = _exp,
+                hp = _hp,
+                score = _score,
+                level = _level,
+                status = _status
+            };
+            return playerData;
+        }
+    */
+    private async UniTask AddRankingDataAsync(string responseBody)
+    {
+        // JSON 응답 파싱
+        JObject jsonResponse = await UniTask.Run(() => JObject.Parse(responseBody));
+        JArray rankingDataArray = (JArray)jsonResponse["rankingData"];
+
+        if (rankingDataArray == null)
+        {
+            Debug.LogError("랭킹 데이터 배열이 비어 있습니다.");
+            return;
+        }
+
+        // 랭킹 데이터를 각 리스트에 추가
+        foreach (JObject rankingData in rankingDataArray)
+        {
+            string id = (string)rankingData["id"];
+            int? score = (int?)rankingData["score"];
+            int? ranking = (int?)rankingData["ranking"];
+
+            if (id != null && score.HasValue && ranking.HasValue)
+            {
+                _rankIdList.Add(id);
+                _rankScoreList.Add(score.Value);
+                _rankingList.Add(ranking.Value);
+                Debug.Log(_rankIdList);
+            }
+            else
+            {
+                Debug.LogWarning("랭킹 데이터에 null이 있거나 값 누락");
+            }
         }
     }
 }
