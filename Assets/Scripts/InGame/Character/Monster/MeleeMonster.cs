84940ffb7aabafe4e2ff93b6962648fe4ce38dc0
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-08-04 14:03:43 +0900

    Revert "defualt monster setting"
    This reverts commit 7b445977e969774cac433a4390f64c66acca0c77.

--- Assets/Scripts/InGame/Character/Monster/MeleeMonster.cs
+++ Assets/Scripts/InGame/Character/Monster/MeleeMonster.cs
@@ -4,31 +4,6 @@ using UnityEngine;
 
 public class MeleeMonster : MonsterBase
 {
-    // temp monster status
-    private int meleeMonster_Level;
-    private int meleeMonster_exp;
-    private float meleeMonster_Hp;
-    private float meleeMonster_Speed;
-    private float meleeMonster_RateOfFire;
-    private float meleeMonster_ProjectileSpeed;
-    private float meleeMonster_CollisionDamage;
-    private int meleeMonster_Score;
-    private float meleeMonster_Range;
-
-    private void Start()
-    {
-        // init melee monster variable
-        // meleeMonster_Level = 0;
-        // meleeMonster_exp = 0;
-        // meleeMonster_Hp = 0;
-        // meleeMonster_Speed = 0;
-        // meleeMonster_RateOfFire = 0;
-        // meleeMonster_ProjectileSpeed = 0;
-        // meleeMonster_CollisionDamage = 0;
-        // meleeMonster_Score = 0;
-        // meleeMonster_Range = 0;
-    }
-
     protected override void OnDisable()
     {
         base.OnDisable();
