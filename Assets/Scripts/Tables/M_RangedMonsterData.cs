c8b5a10e6f199159107732e92ff5c94b06cec40e
Wally0822 <1506325470piao@gmail.com>
2023-08-09 19:07:04 +0900

    Add JsonData Load

---
+++ Assets/Scripts/Tables/M_RangedMonsterData.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class M_RangedMonsterData
+{
+    public int Id;
+    public string type;
+    public int level;
+    public int exp;
+    public float hp;
+    public float speed;
+    public float rate_of_fire;
+    public float projectile_speed;
+    public float collision_damage;
+    public int score;
+    public float ranged;
+    public static Dictionary<int, M_RangedMonsterData> table = new Dictionary<int, M_RangedMonsterData> ();   
+}
