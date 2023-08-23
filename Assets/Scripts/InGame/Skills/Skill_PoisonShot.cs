499304ff3638741cc0642f514357fe7f740834b2
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-08-23 23:42:22 +0900

    input Skill Base Scripts
    NerfShot ~ NapalmShot

---
+++ Assets/Scripts/InGame/Skills/Skill_PoisonShot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_PoisonShot : SkillBase
+{
+    public override void SkillCoolTime()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SkillLevelUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SkillShot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
