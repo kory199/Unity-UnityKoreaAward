d1a572dd7bb0d408c13c0acf3c699400b9cd46b6
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-08-26 18:15:05 +0900

    add skill
    rapid shot

--- Assets/Scripts/InGame/Bullet/Bullet_NerfShot.cs
+++ Assets/Scripts/InGame/Bullet/Bullet_NerfShot.cs
@@ -10,21 +10,9 @@ public class Bullet_NerfShot : MonoBehaviour
 
     private float damageReduction;
 
-
     private void Awake()
     {
         NerfShotInit();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void NerfShotInit()
@@ -78,7 +66,7 @@ public class Bullet_NerfShot : MonoBehaviour
             }
             else
             {
-
+                // 다른 몬스터 종류
             }
         }
         else if (collision.gameObject.tag == "Wall")
@@ -87,7 +75,7 @@ public class Bullet_NerfShot : MonoBehaviour
         }
         else
         {
-            
+            // 다른 오브젝트 추가 시
         }
     }
 }
--- Assets/Scripts/InGame/Bullet/Bullet_RapidShot.cs
+++ Assets/Scripts/InGame/Bullet/Bullet_RapidShot.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Bullet_RapidShot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D bulletRb;
+    CircleCollider2D bulletCollider2D;
+    MeleeMonster meleeMonster;
+
+
+    private void Awake()
     {
-        
+        NerfShotInit();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void NerfShotInit()
     {
-        
+        if (gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody2D))
+        {
+            bulletRb = rigidbody2D;
+        }
+        else
+        {
+            bulletRb = gameObject.AddComponent<Rigidbody2D>();
+        }
+
+        if (gameObject.TryGetComponent<CircleCollider2D>(out CircleCollider2D circleCollider2D))
+        {
+            bulletCollider2D = circleCollider2D;
+        }
+        else
+        {
+            gameObject.AddComponent<CircleCollider2D>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            if (collision.gameObject.name == "BossOne")
+            {
+                // 보스 로직 추가 필요
+            }
+            else if (collision.gameObject.name == "BasicMeleeMonster")
+            {
+                if (collision.gameObject.TryGetComponent<MeleeMonster>(out MeleeMonster meleeMonster))
+                {
+                    this.meleeMonster = meleeMonster;
+                }
+                else
+                {
+                    meleeMonster = collision.gameObject.AddComponent<MeleeMonster>();
+                }
+
+                meleeMonster.Hit();
+                Destroy(gameObject);
+            }
+            else
+            {
+                // 다른 몬스터 종류 추가 시
+            }
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 다른 오브젝트 추가 시
+        }
     }
 }
