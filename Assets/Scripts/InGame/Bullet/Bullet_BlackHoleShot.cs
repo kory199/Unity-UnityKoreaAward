73ecbb4f27a3e5f9ec658b2643887c0c9a4a2828
ShineSungHwan <97496901+kory199@users.noreply.github.com>
2023-08-29 02:49:17 +0900

    blackhole skill -ing

---
+++ Assets/Scripts/InGame/Bullet/Bullet_BlackHoleShot.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet_BlackHoleShot : MonoBehaviour
+{
+    // Bullet info
+    private Rigidbody2D bulletRb;
+    private CircleCollider2D bulletCollider2D;
+    private MeleeMonster meleeMonster;
+    private float lifeTime;
+
+    // child info
+    private GameObject blackHole;
+    Skill_BlackHoleChild skill_BlackHoleChild;
+    Vector3 maxScale;
+    Vector3 originScale;
+    Vector3 increaseScale;
+
+    #region Unity Life Cycle
+    private void Awake()
+    {
+        BlackHoleShotInit();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        lifeTime = 3f;
+    }
+    #endregion
+
+    private void BlackHoleShotInit()
+    {
+        if (gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody2D))
+        {
+            bulletRb = rigidbody2D;
+        }
+        else
+        {
+            bulletRb = gameObject.AddComponent<Rigidbody2D>();
+        }
+
+        if (gameObject.TryGetComponent<CircleCollider2D>(out CircleCollider2D circleCollider2D))
+        {
+            bulletCollider2D = circleCollider2D;
+        }
+        else
+        {
+            gameObject.AddComponent<CircleCollider2D>();
+        }
+
+        blackHole = gameObject.transform.GetChild(0).gameObject;
+
+        if (blackHole.TryGetComponent<Skill_BlackHoleChild>(out Skill_BlackHoleChild blackholeChild))
+        {
+            skill_BlackHoleChild = blackholeChild;
+        }
+        else
+        {
+            skill_BlackHoleChild = blackHole.AddComponent<Skill_BlackHoleChild>();
+        }
+
+        originScale = new Vector3(0.1f, 0.1f, 0.1f);
+        maxScale = Vector3.one;
+        increaseScale = new Vector3(0.05f, 0.05f, 0.05f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            if (collision.gameObject.name == "BossOne")
+            {
+                // 보스 로직 추가 필요
+            }
+            else if (collision.gameObject.name == "BasicMeleeMonster")
+            {
+                if (collision.gameObject.TryGetComponent<MeleeMonster>(out MeleeMonster meleeMonster))
+                {
+                    this.meleeMonster = meleeMonster;
+                }
+                else
+                {
+                    meleeMonster = collision.gameObject.AddComponent<MeleeMonster>();
+                }
+
+                meleeMonster.Hit();
+                StartCoroutine(skill_BlackHoleChild.CreateBlackHole(originScale, maxScale, increaseScale));
+                Destroy(gameObject, lifeTime);
+            }
+            else
+            {
+                // 다른 몬스터 종류 추가 시
+            }
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 다른 오브젝트 추가 시
+        }
+    }
+
+    public void SetBlackHoleScale(int skillLevel)
+    {
+        maxScale *= skillLevel * 0.2f;
+    }
+}
---
+++ Assets/Scripts/InGame/Skills/Skill_BlackHoleChild.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Skill_BlackHoleChild : MonoBehaviour
+{
+    CircleCollider2D blackHoleCollider;
+
+    private void Awake()
+    {
+        if (gameObject.TryGetComponent<CircleCollider2D>(out CircleCollider2D circleCollider2D))
+        {
+            blackHoleCollider = circleCollider2D;
+        }
+        else
+        {
+            blackHoleCollider = gameObject.AddComponent<CircleCollider2D>();
+        }
+    }
+
+    public IEnumerator CreateBlackHole(Vector3 originScale, Vector3 maxScale, Vector3 increaseScale)
+    {
+        while (originScale.x < maxScale.x)
+        {
+            gameObject.transform.localScale = originScale;
+
+            originScale += increaseScale;
+
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            if (collision.gameObject.name == "BasicMeleeMonster")
+            {
+                Rigidbody2D monsterTransform = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            }
+        }
+    }
+
+    private IEnumerator BlackHoleEntry(Rigidbody2D monsterRb)
+    {
+        while (true)
+        {
+
+
+            yield return null;
+        }
+    }
+}
