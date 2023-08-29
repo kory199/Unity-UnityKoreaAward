cfeb453d661510fc07d08c78c5f9b3e19bf1c62e
Wally0822 <1506325470piao@gmail.com>
2023-08-29 17:03:07 +0900

    Update EnhancePanel

--- Assets/Scripts/SystemBase/StageManager.cs
+++ Assets/Scripts/SystemBase/StageManager.cs
@@ -95,7 +95,7 @@ public class StageManager : MonoSingleton<StageManager>
         }
 
         Debug.Log("Stage Up ...");
-        
+
         _uI_Enhance.GetSkillPoint(_stageNum);
         _uI_Enhance.OnShow();
 
@@ -145,7 +145,6 @@ public class StageManager : MonoSingleton<StageManager>
             _stageNum++;
             _uI_SceneGame.SetStageNum(_stageNum);
             _uI_SceneGame.SetLevel(_stageNum);
-            StartCoroutine(InitUI_LevelUp());
         }
     }
 
@@ -248,13 +247,5 @@ public class StageManager : MonoSingleton<StageManager>
     {
         _uI_Enhance = UIManager.Instance.CreateObject<UI_Enhance>("UI_Enhance", EnumTypes.LayoutType.Middle);
         _uI_Enhance.OnHide();
-    }
-
-    IEnumerator InitUI_LevelUp()
-    {
-        UI_LevelUp uI_LevelUp = UIManager.Instance.CreateObject<UI_LevelUp>("Popup_LevelUp", EnumTypes.LayoutType.Middle);
-        uI_LevelUp.OnShow();
-        yield return new WaitForSeconds(1f);
-        uI_LevelUp.OnHide();
     }
 }
--- Assets/Scripts/UI/UI_LevelUp.cs
+++ Assets/Scripts/UI/UI_LevelUp.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using System;
 
 public class UI_LevelUp : UIBase
 {
@@ -11,6 +13,13 @@ public class UI_LevelUp : UIBase
     private void OnEnable()
     {
         Time.timeScale = 0;
+        OnShowTime();
+    }
+
+    private async void OnShowTime()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(0.5));
+        OnHide();
     }
 
     private void OnDisable()
